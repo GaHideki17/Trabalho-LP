334c5504b3e19278bd5d10e37b252dc4913a161c
Tho-hen <thomas.henschel123@gmail.com>
2025-06-11 00:55:31 -0300

    Inserir Dados

--- db/dados.js
+++ db/dados.js
@@ -37,7 +37,118 @@
       "cep": "21035-070"
     }
   ],
-  "fornecedores": [],
-  "produtos": [],
-  "usuarios": []
+  "fornecedores": [
+    {
+      "id": "1",
+      "cpf": "111.111.111-11",
+      "nome": "Chevrolet",
+      "telefone": "(18)9111-1111",
+      "cidade": "Presidente Prudente",
+      "uf": "SP",
+      "cep": "19015-010"
+    },
+    {
+      "id": "2",
+      "cpf": "222.222.222-22",
+      "nome": "Toyota",
+      "telefone": "(18)9222-2222",
+      "cidade": "Londrina",
+      "uf": "PR",
+      "cep": "14023-013"
+    },
+    {
+      "id": "3",
+      "cpf": "333.333.333-33",
+      "nome": "Renault",
+      "telefone": "(11)9333-3333",
+      "cidade": "São Paulo",
+      "uf": "SP",
+      "cep": "11033-029"
+    },
+    {
+      "id": "4",
+      "cpf": "444.444.444-44",
+      "nome": "Ford",
+      "telefone": "(17)9444-4444",
+      "cidade": "Três Lagoas",
+      "uf": "MS",
+      "cep": "21035-070"
+    }
+  ],
+  "produtos": [
+    {
+      "Nome": "Kwid",
+      "ano": "2018",
+      "modelo": "hatch subcompacto",
+      "cor": "Preto",
+      "QTD": "1",
+      "marca": "Renault"
+    },
+    {
+      "Nome": "Polo",
+      "ano": "2018",
+      "modelo": "Compacto premium",
+      "cor": "prata",
+      "QTD": "1",
+      "marca": "VolksWagen"
+    },
+    {
+      "Nome": "Camaro",
+      "ano": "2022",
+      "modelo": "Muscle car",
+      "cor": "Amarelo",
+      "QTD": "1",
+      "marca": "Chevrolet"
+    }
+  ],
+  "categoria": [
+    {
+      "nome": "Muscle car",
+      "descricao": "Mudar"
+    },
+    {
+      "nome": "Compacto",
+      "descricao": "Mudar"
+    },
+    {
+      "nome": "Sub Compacto",
+      "descricao": "Mudar"
+    },
+    {
+      "nome": "Eletrico",
+      "descricao": "Mudar"
+    }
+  ],
+    "entregadores": [{
+
+      "cpf": "111.111.111-11",
+      "nome": "João Carlos Gouveia",
+      "data": "xx/xx/xxxx",
+      "rg": "123555396534",
+      "cep": "19015-010"
+    },
+    {
+
+      "cpf": "123.1231.154-11",
+      "nome": "Thomas",
+      "data": "xx/xx/xxxx",
+      "rg": "132652334",
+      "cep": "19325-023"
+    },
+    {
+      "cpf": "453.186.117-11",
+      "nome": "Gabriel Hideki",
+      "data": "xx/xx/xxxx",
+      "rg": "126534596534",
+      "cep": "19015-233"
+    },
+    {
+      
+      "cpf": "763.144.123-44",
+      "nome": "Henrique",
+      "data": "xx/xx/xxxx",
+      "rg": "1288996534",
+      "cep": "19015-666"
+    }]
+
 }
