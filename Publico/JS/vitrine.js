666e30e6266453464ec7ca36745f7222ace8bba8
Tho-hen <thomas.henschel123@gmail.com>
2025-06-11 01:05:09 -0300

    Js da Vitrine

--- Publico/JS/vitrine.js
+++ Publico/JS/vitrine.js
@@ -1,29 +1,24 @@
-function carregarProdutos(){
-    fetch('https://fakestoreapi.com/products',{
-        method:"GET"
-    }).then((resposta) =>{
-        if (resposta.ok){
-            return resposta.json();
-        }
-    }).then((listaDeProdutos) => {
-        const divVitrine = document.getElementById("vitrine");
-        for (const produto of listaDeProdutos){
-            let card = document.createElement('div');
-            card.innerHTML=`
-            <div class="card" style="width: 18rem;">
-                <img width="150px" height="250px" src="${produto.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${produto.title}</h5>
-                    <p class="card-text">${produto.price}</p>
-                    <a href="#" class="btn btn-primary">Comprar</a>
-                </div>
+function carregarProdutos() {
+fetch('http://localhost:3000/produtos')
+  .then(res => res.json())
+  .then(produtos => {
+    const vitrine = document.getElementById("vitrine");
+    for (const produto of produtos) {
+        let card = document.createElement('div');
+        card.classList.add("card", "m-2");
+        card.style.width = "18rem";
+        card.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${produto.Nome}</h5>
+                <p class="card-text">Modelo: ${produto.modelo}</p>
+                <p class="card-text">Ano: ${produto.ano}</p>
+                <p class="card-text">Cor: ${produto.cor}</p>
+                <p class="card-text">Marca: ${produto.marca}</p>
+                <a href="#" class="btn btn-primary">Comprar</a>
             </div>
-            `;
-            divVitrine.appendChild(card);
-        }
-    }).catch((erro)=>{
-        alert("Não foi possível carregar os produtos para a vitrine:" + erro);
-    });
-}
+        `;
+        vitrine.appendChild(card);
+    }
+  })};
 
-carregarProdutos();
+  carregarProdutos();
