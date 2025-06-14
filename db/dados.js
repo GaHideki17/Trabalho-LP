2832e9406162ba69e9915e8e4fa30bb2da2e2273
GaHideki17 <gabrielhidekisuguiyama@gmail.com>
2025-06-14 20:56:45 -0300

    Teste

--- db/dados.js
+++ db/dados.js
@@ -1,39 +1,36 @@
+{
+  "clientes": [
+    { "id": "1", "cpf": "111.111.111-11", "nome": "João Carlos Gouveia", "telefone": "(18)9111-1111", "cidade": "Presidente Prudente", "uf": "SP", "cep": "19015-010" },
+    { "id": "2", "cpf": "222.222.222-22", "nome": "Odete Roitman", "telefone": "(18)9222-2222", "cidade": "Londrina", "uf": "PR", "cep": "14023-013" },
+    { "id": "3", "cpf": "333.333.333-33", "nome": "Sandra Regina Vasconcelos", "telefone": "(11)9333-3333", "cidade": "São Paulo", "uf": "SP", "cep": "11033-029" },
+    { "id": "4", "cpf": "444.444.444-44", "nome": "Jason Grace", "telefone": "(17)9444-4444", "cidade": "Três Lagoas", "uf": "MS", "cep": "21035-070" }
+  ],
 
-"clientes": [
-        { "id": "1", "cpf": "111.111.111-11", "nome": "João Carlos Gouveia", "telefone": "(18)9111-1111", "cidade": "Presidente Prudente", "uf": "SP", "cep": "19015-010" },
-        { "id": "2", "cpf": "222.222.222-22", "nome": "Odete Roitman", "telefone": "(18)9222-2222", "cidade": "Londrina", "uf": "PR", "cep": "14023-013" },
-        { "id": "3", "cpf": "333.333.333-33", "nome": "Sandra Regina Vasconcelos", "telefone": "(11)9333-3333", "cidade": "São Paulo", "uf": "SP", "cep": "11033-029" },
-        { "id": "4", "cpf": "444.444.444-44", "nome": "Jason Grace", "telefone": "(17)9444-4444", "cidade": "Três Lagoas", "uf": "MS", "cep": "21035-070" }
-      ],
+  "fornecedores": [
+    { "id": "1", "cpf": "111.111.111-11", "nome": "Chevrolet", "telefone": "(18)9111-1111", "cidade": "Presidente Prudente", "uf": "SP", "cep": "19015-010" },
+    { "id": "2", "cpf": "222.222.222-22", "nome": "Toyota", "telefone": "(18)9222-2222", "cidade": "Londrina", "uf": "PR", "cep": "14023-013" },
+    { "id": "3", "cpf": "333.333.333-33", "nome": "Renault", "telefone": "(11)9333-3333", "cidade": "São Paulo", "uf": "SP", "cep": "11033-029" },
+    { "id": "4", "cpf": "444.444.444-44", "nome": "Ford", "telefone": "(17)9444-4444", "cidade": "Três Lagoas", "uf": "MS", "cep": "21035-070" },
+    { "id": "5", "cpf": "555.555.555-55", "nome": "VolksWagen", "telefone": "(19)9555-5555", "cidade": "Campinas", "uf": "SP", "cep": "13040-000" }
+  ],
 
-  
-"fornecedores": [
-        { "id": "1", "cpf": "111.111.111-11", "nome": "Chevrolet", "telefone": "(18)9111-1111", "cidade": "Presidente Prudente", "uf": "SP", "cep": "19015-010" },
-        { "id": "2", "cpf": "222.222.222-22", "nome": "Toyota", "telefone": "(18)9222-2222", "cidade": "Londrina", "uf": "PR", "cep": "14023-013" },
-        { "id": "3", "cpf": "333.333.333-33", "nome": "Renault", "telefone": "(11)9333-3333", "cidade": "São Paulo", "uf": "SP", "cep": "11033-029" },
-        { "id": "4", "cpf": "444.444.444-44", "nome": "Ford", "telefone": "(17)9444-4444", "cidade": "Três Lagoas", "uf": "MS", "cep": "21035-070" }
-      ];
+  "produtos": [
+    { "nome": "Kwid", "ano": "2018", "modelo": "Sub Compacto", "cor": "Preto", "qtd": "1", "marca": "Renault" },
+    { "nome": "Polo", "ano": "2018", "modelo": "Compacto", "cor": "Prata", "qtd": "1", "marca": "VolksWagen" },
+    { "nome": "Camaro", "ano": "2022", "modelo": "Muscle car", "cor": "Amarelo", "qtd": "1", "marca": "Chevrolet" }
+  ],
 
-    
-"produtos": [
-        { "nome": "Kwid", "ano": "2018", "modelo": "hatch subcompacto", "cor": "Preto", "qtd": "1", "marca": "Renault" },
-        { "nome": "Polo", "ano": "2018", "modelo": "Compacto premium", "cor": "prata", "qtd": "1", "marca": "VolksWagen" },
-        { "nome": "Camaro", "ano": "2022", "modelo": "Muscle car", "cor": "Amarelo", "qtd": "1", "marca": "Chevrolet" }
-      ],
-    
+  "categorias": [
+    { "nome": "Muscle car", "descricao": "Carros potentes de alto desempenho com design esportivo." },
+    { "nome": "Compacto", "descricao": "Veículos pequenos ideais para uso urbano." },
+    { "nome": "Sub Compacto", "descricao": "Veículos menores que os compactos, com excelente economia de combustível." },
+    { "nome": "Elétrico", "descricao": "Veículos movidos por motores elétricos e baterias recarregáveis." }
+  ],
 
-"categorias": [
-        { "nome": "Muscle car", "descricao": "Mudar" },
-        { "nome": "Compacto", "descricao": "Mudar" },
-        { "nome": "Sub Compacto", "descricao": "Mudar" },
-        { "nome": "Eletrico", "descricao": "Mudar" }
-      ],
-  
-
-    
-"entregadores": [
-        { "cpf": "111.111.111-11"," nome": "João Carlos Gouveia", "data": "xx/xx/xxxx", "rg": "123555396534", "cep": "19015-010" },
-        { "cpf": "123.1231.154-11", "nome": "Thomas", "data": "xx/xx/xxxx", "rg": "132652334", "cep": "19325-023" },
-        { "cpf": "453.186.117-11", "nome": "Gabriel Hideki", "data": "xx/xx/xxxx", "rg": "126534596534", "cep": "19015-233" },
-        { "cpf": "763.144.123-44", "nome": "Henrique", "data": "xx/xx/xxxx", "rg": "1288996534", "cep": "19015-666" }
-      ],
+  "entregadores": [
+    { "cpf": "111.111.111-11", "nome": "João Carlos Gouveia", "data": "14/03/2023", "rg": "123555396534", "cep": "19015-010" },
+    { "cpf": "123.123.154-11", "nome": "Thomas", "data": "22/07/2022", "rg": "132652334", "cep": "19325-023" },
+    { "cpf": "453.186.117-11", "nome": "Gabriel Hideki", "data": "05/01/2024", "rg": "126534596534", "cep": "19015-233" },
+    { "cpf": "763.144.123-44", "nome": "Henrique", "data": "30/11/2023", "rg": "1288996534", "cep": "19015-666" }
+  ]
+}
